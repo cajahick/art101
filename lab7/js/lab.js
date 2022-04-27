c370f542b1adc8187019574f8b3063296a0d5585
cajahick <102631089+cajahick@users.noreply.github.com>
2022-04-27 14:02:52 -0700

    lab 7 update

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -6,12 +6,12 @@
 
 function userNameShuffle() {
     //Get user input.
-  var userName = window.prompt();
+  var userName = window.prompt("What's your name?");
     //Sort the letters by splitting, sorting, and joining the letters again.
   sortedLetters = userName.toLowerCase().split("").sort().join("");
     //Return the sorted name.
   return sortedLetters;
   }
 
-document.writeln("And now your name is more 'unique': ",
+document.writeln("New Name: ",
       userNameShuffle(), "</br>")
