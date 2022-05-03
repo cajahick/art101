993da3cf0d571df89ed76abf50e3e88f1ed4c711
cajahick <102631089+cajahick@users.noreply.github.com>
2022-05-02 17:10:13 -0700

    lab 8

---
+++ lab8/js/lab.js
@@ -0,0 +1,25 @@
+/*
+ * Author: Cassie Hicks
+ * Created: May 2, 2022
+ */
+
+function add(x) {
+  var results = x * 2
+  return results;
+}
+
+
+console.log("5 doubled: " + add(5));
+console.log("6 doubled: " + add(6));
+
+var array = [8, 6, 7, 5, 3, 0, 9]
+
+console.log("Array doubled: " + array.map(squared));
+
+array.map(function(x) {
+  var results = x * 3
+  return results;
+
+})
+
+console.log("Double array: " result);
